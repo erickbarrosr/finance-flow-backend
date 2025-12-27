c4927b53f18df13e4a65259413275a9d950f3eb5
Erick Rezende <erickbarrosrezende@gmail.com>
2025-12-27 17:50:30 -0300

    feat: Create UserEntity

---
+++ src/main/java/br/com/ersoftwares/financeflow/user/dataprovider/entity/UserEntity.java
@@ -0,0 +1,35 @@
+package br.com.ersoftwares.financeflow.user.dataprovider.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
