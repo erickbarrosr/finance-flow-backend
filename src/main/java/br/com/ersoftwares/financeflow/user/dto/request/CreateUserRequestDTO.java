12119cb4b3bdf95067df8722b12da885d6603d18
Erick Rezende <erickbarrosrezende@gmail.com>
2025-12-28 11:04:03 -0300

    feat: Create UserRequestDTO

---
+++ src/main/java/br/com/ersoftwares/financeflow/user/dto/request/CreateUserRequestDTO.java
@@ -0,0 +1,25 @@
+package br.com.ersoftwares.financeflow.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateUserRequestDTO {
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String name;
+
+    @Email(message = "Email deve ser válido")
+    @NotBlank(message = "Email é obrigatório")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    private String password;
+}
